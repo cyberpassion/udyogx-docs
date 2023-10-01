ee2576fe6dff884d3bf43676188bdb16da6e8803
Ravi Sharma <ravi26march@gmail.com>
2023-10-01 13:41:00 +0530

    sasa

--- components/cards.tsx
+++ components/cards.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 function CardButton(props) {
   return (
     <div style="background: white;border-radius: 0.5em;border: 1px solid #f3eded;box-shadow: 0 1px 2px rgba(0,0,0,0.04);margin:1.25% 0.5% 0 0;width:48%;">
-      <a href={props.href} style="display:block;padding: 0.5em 1em;"></><button>
-        {props.title}
+      <a href={props.href} style="display:block;padding: 0.5em 1em;">
+      <button>  {props.title}
       </button></a>
     </div>
   );
