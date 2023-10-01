3ac971522acf38d3089771e4eee5908f518d8724
Ravi Sharma <ravi26march@gmail.com>
2023-10-01 13:08:45 +0530

    Update cards.tsx

--- components/cards.tsx
+++ components/cards.tsx
@@ -2,7 +2,7 @@ const Cards = (items:any) => {
   return (
     <>
       {items.map((item:any)=>{
-        return (<div xs={{'boxShadow':'0 1px 2px 0 rgba(0,0,0,0.05)'}}>{item.title}</div>)
+        return (<div>{item.title}</div>)
       })}
     </>
   );
