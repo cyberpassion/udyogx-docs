666e76382ac04fa2fa2b21ed3adeb6709bdb3e7d
Ravi Sharma <ravi26march@gmail.com>
2023-10-01 13:37:56 +0530

    dsdsds

--- components/cards.tsx
+++ components/cards.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 function CardButton(props) {
   return (
-    <div>
+    <div style="background: white;border-radius: 0.5em;padding: 0.5em 1em;border: 1px solid #f3eded;box-shadow: 0 1px 2px rgba(0,0,0,0.04);margin:1.25% 0.5% 0 0;width:48%;">
       <button>
         {props.title}
       </button>
