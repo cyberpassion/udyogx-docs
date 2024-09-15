6e15ef14bd6def55004376806f02ef7950f83766
Ravi Sharma <ravi26march@gmail.com>
2024-09-15 13:13:10 +0530

    updates

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+// pages/_app.js
+import '../styles/global.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({
+  weight: ['400', '500', '700', '900'], // Choose the weights you want to include
+  subsets: ['latin'],            // Define character sets
+  display: 'swap',               // Optional for faster font rendering
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={inter.className}> {/* Apply the font globally */}
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+import { useRouter } from 'next/router'
+import _meta from './pages/_meta.json'
+
 const config: DocsThemeConfig = {
-  logo: <span>UdyogX</span>,
+  logo: <>
+	<img src='../images/udx-logo-yellow.png' style={{ width: '32px' }}></img><span className="nx-ml-2 nx-font-bold hidden md:inline">{_meta.brand_name}</span>
+  </>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: _meta.brand_docs_github_link,
   },
   chat: {
-    link: 'https://discord.com',
+    link: _meta.brand_support_link,
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: _meta.brand_docs_github_link,
   footer: {
-    text: 'Nextra Docs Template',
+    text: `${_meta.brand_name} Docs`,
   },
+  // Remove default - Nextra from end of title to our own
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: `%s – ${_meta.brand_name}`
+      }
+    }
+  }
 }
 
 export default config
