c43bd1575c5912a72ac19a083ae1d275e0bd7ab1
Ravi Sharma <ravi26march@gmail.com>
2024-09-15 23:37:23 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import '../styles/global.css';
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font';
 
 const inter = Inter({
   weight: ['400', '500', '700', '900'], // Choose the weights you want to include
